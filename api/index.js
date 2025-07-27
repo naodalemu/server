1cdc2aabfcc19b15159744441d81ff7be21cc004
Naod Alemu <naodalemu11@gmail.com>
2025-07-27 18:07:13 +0300

    updated header stuff

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const corsMiddleware = cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
 });
 
 async function runCorsMiddleware(req, res) {
