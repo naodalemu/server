46c2aefab236b90ee5fa92944815ce7c1b9f2985
Naod Alemu <naodalemu11@gmail.com>
2025-07-27 20:21:12 +0300

    failed to fix authoraization, another try

--- api/index.js
+++ api/index.js
@@ -39,9 +39,7 @@ async function relayRequestWithPuppeteer(path, method, body, headers) {
         console.log('Navigating to base URL to solve security challenge...');
 
         // Give the page up to 60 seconds to load to prevent timeouts
-        await page.goto(baseApiUrl, { timeout: 60000 });
-        // Wait for a few seconds to ensure any client-side scripts have run
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await page.goto(baseApiUrl, { waitUntil: 'networkidle0', timeout: 15000 });
 
         console.log('Security challenge passed, cookie should be set.');
 
@@ -51,7 +49,6 @@ async function relayRequestWithPuppeteer(path, method, body, headers) {
                 const headers = {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest',
                 };
 
                 if (authorizationHeader) {
